3c1535ce6a865a54c3d01aa0846fb38067bdb7ec
MBarrr <62114563+MBarrr@users.noreply.github.com>
2022-05-20 10:49:05 +0100

    +

---
+++ src/main/java/mbarrr/github/emberlib/Util/UTIL.java
@@ -0,0 +1,27 @@
+package mbarrr.github.emberlib.Util;
+
+import org.bukkit.Location;
+
+public class UTIL {
+
+    /**
+     * Return whether a location is between two locations
+     * @param firstBound First bound
+     * @param secondBound Second bound
+     * @param location Location to test
+     * @return true if location is between the two points, otherwise false
+     */
+    public static boolean isLocationInBound(Location firstBound, Location secondBound, Location location){
+
+        double x1 = firstBound.getX();
+        double y1 = firstBound.getY();
+        double z1 = firstBound.getZ();
+
+        double x2 = secondBound.getX();
+        double y2 = secondBound.getY();
+        double z2 = secondBound.getZ();
+
+        return (location.getX() > x1) && (location.getY() > y1) && (location.getZ() > z1) && (location.getX() < x2) && (location.getY() < y2) && (location.getZ() < z2);
+
+    }
+}
